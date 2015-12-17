d0280309aa6c01c257137452f6a1fcbcc5f9cad5
Carlos Rojas <carlos.sadhu@gmail.com>
2015-12-17 00:45:44 -0500

    Allows todo to be completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214025256) do
+ActiveRecord::Schema.define(version: 20151217052710) do
 
   create_table "todos", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.datetime "completed_at"
   end
 
 end
