a9dd7027e2e5d0c3f14f1e75d86b500f10b5e09e
Carlos Rojas <carlos.sadhu@gmail.com>
2015-12-20 17:33:15 -0500

    Refactor tests to use helper methods

--- spec/features/user_completes_todo_spec.rb
+++ spec/features/user_completes_todo_spec.rb
@@ -4,11 +4,11 @@ feature "User completes a todo" do
   scenario "sucessfully" do
     sign_in
     
-    click_on 'Add a new todo'
-    fill_in 'Title', with: 'Buy milk'
-    click_on 'Submit'
+    create_todo("Buy milk")
 
     click_on "Mark complete"
-    expect(page).to have_css '.todos li.completed', text: 'Buy milk'
+    expect(page).to display_completed_todo 'Buy milk'
   end
+  
+  
 end
--- spec/features/user_sees_own_todos_spec.rb
+++ spec/features/user_sees_own_todos_spec.rb
@@ -4,6 +4,6 @@ feature "User sees own todos" do
   scenario "doesn't see others' todos" do
     Todo.create! title: 'Buy milk', email: 'someone_else@example.com'
     sign_in_as 'someone@example.com'
-    expect(page).not_to have_css '.todos li', text: 'Buy milk'
+    expect(page).not_to display_todo 'Buy milk'
   end
 end
